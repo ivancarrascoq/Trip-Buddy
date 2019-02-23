ff591937c40f12ea5f076ce7b7215fb029d4160e
ivancarrascoq <ivancarrascoq@gmail.com>
2019-02-23 13:07:50 -0600

    added created_at, updated_at to trips

--- main_app/models.py
+++ main_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.core.validators import MinLengthValidator
 from django.contrib.auth.models import User
 from django.conf import settings
-
+from django.utils.timezone import datetime
 
 # Create your models here.
 class Trip(models.Model):
@@ -11,7 +11,8 @@ class Trip(models.Model):
     start_date = models.DateField(null=False)
     end_date = models.DateField(null=False)
     plan = models.CharField(validators=[MinLengthValidator(3)], null=False, max_length=200)
-
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 class TripJoined(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     trip = models.ForeignKey('Trip', on_delete=models.CASCADE)
